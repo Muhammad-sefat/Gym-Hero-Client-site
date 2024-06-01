9b93c5140309bae85062d203a39caadf80603d35
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-06-01 16:01:27 +0600

    Complete Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
-      <p>hello</p>
-      <Outlet></Outlet>
+      <div className="pt-24 min-h-[calc(100vh-68px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </>
   );
 }
