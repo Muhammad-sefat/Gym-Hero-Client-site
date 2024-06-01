7f673ae6f93b15c2c53097838c914b0059b73cfd
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-06-01 15:33:41 +0600

    Setup Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
+      <p>hello</p>
       <Outlet></Outlet>
     </>
   );
---
+++ src/Components/Provider/Firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbeRSdphiYr_TBLw1HfptZR8IOkPhQ2ws",
+  authDomain: "gym-hero-client.firebaseapp.com",
+  projectId: "gym-hero-client",
+  storageBucket: "gym-hero-client.appspot.com",
+  messagingSenderId: "401896481349",
+  appId: "1:401896481349:web:01ea44ddef23742702792c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
